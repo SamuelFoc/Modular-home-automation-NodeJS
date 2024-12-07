2698bc8da2d94b17fbaa460bdd073deff572f07e
SamuelFoc <samo.sipikal@gmail.com>
2024-12-07 18:32:46 +0100

    Create sensoric api server file

---
+++ api/sensoric/server.mjs
@@ -0,0 +1,23 @@
+import express from "express";
+import sensoricApiRoutes from "./routes/sensoricApiRoutes.mjs";
+
+// Sensoric data collecting API
+const s_app = express();
+
+// Middleware to parse JSON bodies
+s_app.use(express.json());
+
+// Sensoric API routes
+s_app.use("/sensoric_api", sensoricApiRoutes);
+
+// Run Sensoric API
+const startSensoricAPI = () => {
+  s_app.listen(process.env?.S_PORT || 3333, () => {
+    console.log(
+      `Sensoric API running on port ${process.env?.S_PORT || 3333}..`
+    );
+  });
+};
+
+// EXPORT
+export default startSensoricAPI;
