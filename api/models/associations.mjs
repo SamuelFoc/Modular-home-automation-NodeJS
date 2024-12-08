f9360f1fc57fd1abd72bec4889bd7fb5fe03a2f5
SamuelFoc <samo.sipikal@gmail.com>
2024-12-08 16:45:38 +0100

    Associate offline sensors with gpios

--- api/models/associations.mjs
+++ api/models/associations.mjs
@@ -1,16 +1,25 @@
+import Gpio from "./Gpio.mjs";
+import OfflineSensor from "./OfflineSensor.mjs";
 import OnlineSensor from "./OnlineSensor.mjs";
 import SensoricData from "./SensoricData.mjs";
 
-// Define associations
+// Online Sensors and the Sensoric Data
 OnlineSensor.hasMany(SensoricData, {
   foreignKey: "s_id",
-  as: "data",
 });
 
 SensoricData.belongsTo(OnlineSensor, {
   foreignKey: "s_id",
-  as: "online_sensor",
+});
+
+// Offline Sensors and the Gpios
+OfflineSensor.hasMany(Gpio, {
+  foreignKey: "s_id",
+});
+
+Gpio.belongsTo(OfflineSensor, {
+  foreignKey: "s_id",
 });
 
 // Export models with associations
-export { OnlineSensor, SensoricData };
+export { OnlineSensor, SensoricData, OfflineSensor, Gpio };
