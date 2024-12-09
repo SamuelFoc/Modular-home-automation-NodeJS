b7f335d953e204add21157c1df55d519879ed200
SamuelFoc <samo.sipikal@gmail.com>
2024-12-09 15:41:26 +0100

    Remove offline sensor routes from the sensoric server

--- api/sensoric/server.mjs
+++ api/sensoric/server.mjs
@@ -1,6 +1,5 @@
 import express from "express";
 import dataRoutes from "./routes/data.routes.mjs";
-import offlineSensorRoutes from "./routes/offlineSensor.routes.mjs";
 
 // Sensoric data collecting API
 const s_app = express();
@@ -10,7 +9,6 @@ s_app.use(express.json());
 
 // Sensoric API routes
 s_app.use("/api/sensoric/data", dataRoutes);
-s_app.use("/api/sensoric/read", offlineSensorRoutes);
 
 // Run Sensoric API
 const startSensoricAPI = () => {
