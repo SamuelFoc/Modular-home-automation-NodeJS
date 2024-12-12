d7c3f59da999d8ade8748a16e9a355983a3e692d
SamuelFoc <samo.sipikal@gmail.com>
2024-12-12 15:45:09 +0100

    Add testConnection function to the initializeDatabase

--- api/db/initialization.mjs
+++ api/db/initialization.mjs
@@ -1,11 +1,13 @@
-import sequelize from "./index.mjs";
+import sequelize, { testConnection } from "./index.mjs";
 
 const initializeDatabase = async () => {
   try {
-    await sequelize.sync({ alter: true }); // force: true will drop existing tables and recreate them
+    await testConnection(); // Ensure connection is established before proceeding
+    await sequelize.sync({ alter: true }); // Use { alter: true } to avoid dropping tables unnecessarily
     console.log("Database synchronized successfully.");
   } catch (error) {
-    console.error("Error synchronizing the database:", error);
+    console.error("Error initializing the database:", error);
+    process.exit(1); // Exit the process if initialization fails
   }
 };
 
